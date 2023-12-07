c5d2bf7f0e5948211b13554d0eefc9d2f446d528
Renan <zumpchiat@gmail.com>
2023-12-07 15:05:44 -0300

    config header

--- src/routes.js
+++ src/routes.js
@@ -3,11 +3,13 @@ import { Route, Routes, BrowserRouter } from "react-router-dom";
 
 import Home from "./pages/Home";
 import Filme from "./pages/Filme";
+import Header from "./components/Header";
 
 
 function RoutesApp() {
     return (
         <BrowserRouter>
+            <Header />
             <Routes>
                 <Route path="/" element={<Home />} />
                 <Route path="/filme/:id" element={<Filme />} />
