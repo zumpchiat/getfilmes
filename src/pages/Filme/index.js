9e08d94831c06321dde49bc94ae562a9ce0618a0
Renan <zumpchiat@gmail.com>
2023-12-08 16:37:50 -0300

    details

--- src/pages/Filme/index.js
+++ src/pages/Filme/index.js
@@ -1,10 +1,64 @@
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
 
-function Filme(){
-    return(
-        <div>
-            Filmes Detalhes
+import api from "../../services/api";
+
+
+function Filme() {
+
+    const { id } = useParams();
+    const [filme, setFilme] = useState({});
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+
+        async function loadFilme() {
+            await api.get(`/movie/${id}`, {
+                params: {
+                    api_key: "fd2aee32a57bc8b79974f3800795ec98",
+                    language: "pt-br",
+                }
+            })
+                .then((response) => {
+                    setFilme(response.data);
+                    setLoading(false);
+                })
+                .catch(() => {
+                    console.log("Filme não encontrado");
+                })
+        }
+
+        loadFilme();
+        return () => {
+            console.log("COMPONETE DESMONTADO");
+        }
+
+    }, []);
+
+    if (loading) {
+        return (
+            <div className="filme-info">
+                <h1>Carregando detalhes do filme...</h1>
+
+            </div>
+        )
+    }
+
+    return (
+        <div className="filme-info">
+
+            <h1>{filme.title}</h1>
+            <img src={`https://image.tmdb.org/t/p/original/${filme.backdrop_path}`}
+                alt={filme.title} />
+
+                <h3>Sinopse</h3>
+
+                <span>{filme.overview}</span>
+
+                <strong>Avaliação: {filme.vote_average} / 10</strong>
+
         </div>
     )
 }
 
-export default Filme;
+export default Filme; 
