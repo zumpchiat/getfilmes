cf9263071364cf0dffa8c157e38fbcc2cf26ffc1
Renan <zumpchiat@gmail.com>
2023-12-14 15:39:00 -0300

    Delete favorite movies

--- src/pages/Favoritos/index.js
+++ src/pages/Favoritos/index.js
@@ -15,26 +15,36 @@ function Favoritos() {
 
     }, [])
 
+    function excluirFilme(id) {
+
+        let filtroFilmes = filmes.filter((item) => {
+            return (item.id !== id)
+        })
+
+        setFilmes(filtroFilmes);
+        localStorage.setItem("@primeflix", JSON.stringify(filtroFilmes));
+    }
+
     return (
         <div className="meus-filmes">
 
             <h1> Meus filmes favoritos</h1>
-
+            {filmes.length === 0 && <span> Ops!! nenhum filme salvo</span>}
             <ul>
                 {filmes.map((item) => {
-                    return(
+                    return (
                         <li key={item.id}>
                             <span>{item.title}</span>
 
                             <div>
-                                <Link  to={`/filme/${item.id}`}> Ver detalhes </Link>
-                                <button>Deletar</button>
+                                <Link to={`/filme/${item.id}`}> Ver detalhes </Link>
+                                <button onClick={() => { excluirFilme(item.id) }}>Deletar</button>
                             </div>
                         </li>
                     )
                 })}
             </ul>
-        
+
         </div>
     )
 }
